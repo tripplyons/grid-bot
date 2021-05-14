902fe6b7641da0bd4803083c8d9a56b79992173d
Tripp Lyons <tripplyons@gmail.com>
2021-05-14 12:57:04 -0700

    Change how orders are refilled

--- main.py
+++ main.py
@@ -18,18 +18,17 @@ client = Client(keys["API_KEY"], keys["API_SECRET"], keys["API_PASSPHRASE"])
 def now():
     return int(time.time())
 
-buy_levels = []
-sell_levels = []
+levels = []
 
 decimals = settings['decimals']
 
-orders_per_side = settings['orders'] // 2
+max_orders = settings['orders']
 
-for i in range(orders_per_side):
-    buy_levels.append(round(settings['center'] - settings['spacing'] * (i + 0.5), decimals))
-    sell_levels.append(round(settings['center'] + settings['spacing'] * (i + 0.5), decimals))
+for i in range(max_orders // 2):
+    levels.append(round(settings['center'] - settings['spacing'] * (i + 0.5), decimals))
+    levels.append(round(settings['center'] + settings['spacing'] * (i + 0.5), decimals))
 
-last_fill = ''
+levels = np.sort(levels)
 
 def get_price():
     return float(client.get_ticker(pair)['price'])
@@ -46,63 +45,38 @@ def open_sell(level):
         round(settings['order_size'] / level, decimals)
     ))
 
-def check_for_fills():
-    orders = client.get_orders(pair, limit=settings['orders'] * 2, status='active')
+def check_and_order():
+    price = get_price()
+    orders = client.get_orders(pair, limit=max_orders, status='active')
 
     open_buy_orders = {}
     open_sell_orders = {}    
 
-    for key in buy_levels:
+    for key in levels:
         open_buy_orders[key] = False
-    for key in sell_levels:
+    for key in levels:
         open_sell_orders[key] = False
 
+    total_orders = 0
+
     for item in orders['items']:
-        price = float(item['price'])
-        if price > get_price():
+        total_orders += 1
+        if item['side'] == 'sell':
             open_sell_orders[price] = True
         else:
             open_buy_orders[price] = True
     
-    num_sell = len(list(filter(lambda x : float(x['price']) in sell_levels, orders['items'])))
-    num_buy = len(list(filter(lambda x : float(x['price']) in buy_levels, orders['items'])))
-    total = num_sell + num_buy
-    error = orders_per_side * 2 - total
+    for i in range(len(levels) - 1):
+        if levels[i + 1] < price:
+            if not open_buy_orders[levels[i]]:
+                open_buy(levels[i])
+    for i in range(1, len(levels)):
+        if levels[i - 1] > price:
+            if not open_sell_orders[levels[i]]:
+                open_sell(levels[i])
 
-    if get_price() > settings['center']:
-        if get_price() > sell_levels[0]:
-            # refill buys
-            for level in buy_levels:
-                if not open_buy_orders[level]:
-                    open_buy(level)
-                    open_buy_orders[level] = True
-        
-        while error > 0:
-            open_buy(buy_levels[0])
-            error -= 1
-    else:
-        if get_price() < buy_levels[0]:
-            #refill sells
-            for level in sell_levels:
-                if not open_sell_orders[level]:
-                    open_sell(level)
-                    open_sell_orders[level] = True
-
-        while error > 0:
-            open_sell(sell_levels[0])
-            error -= 1
-
-def init_orders():
-    client.cancel_all_orders(symbol=pair)
-    price = get_price()
-    for i in range(orders_per_side):
-        if buy_levels[i] < price:
-            open_buy(buy_levels[i])
-        if sell_levels[i] > price:
-            open_sell(sell_levels[i])
-
-init_orders()
+client.cancel_all_orders(symbol=pair)
 
 while True:
-    time.sleep(5)
-    check_for_fills()
+    check_and_order()
+    time.sleep(20)
